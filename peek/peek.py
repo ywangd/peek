cde1be72174cf3c084c2a186f78c05ced82273b0
Yang Wang <ywangd@gmail.com>
2020-07-23 01:19:41 +1000

    WIP: add ctrl-d

--- peek/peek.py
+++ peek/peek.py
@@ -1,17 +1,35 @@
 """Main module."""
-from prompt_toolkit import prompt
+from prompt_toolkit import PromptSession
+from prompt_toolkit.key_binding import KeyBindings
 from prompt_toolkit.styles import style_from_pygments_cls
 from prompt_toolkit.lexers import PygmentsLexer
 
 from pygments.styles.default import DefaultStyle
 from pygments.lexers.javascript import JavascriptLexer
 
+kb = KeyBindings()
+# @kb.add('c-d')
+# def _(event):
+#     pass
 
 class Repl:
 
     def __init__(self):
-        pass
+        self.session = PromptSession(key_bindings=kb)
+        self.state_payload = False
 
     def run(self):
-        source = prompt('', lexer=PygmentsLexer(JavascriptLexer), style=style_from_pygments_cls(DefaultStyle))
-        print(f'You typed: {source}')
+        while True:
+            try:
+                self._loop()
+            except EOFError:
+                exit(0)
+
+    def _loop(self):
+        source = self.session.prompt(
+            '', lexer=PygmentsLexer(JavascriptLexer),
+            style=style_from_pygments_cls(DefaultStyle),
+            multiline=self.state_payload,
+        )
+        print(f'You typed: {type(source)} {source}')
+        self.state_payload = not self.state_payload
