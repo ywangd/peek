5f4e38e0d2526b3c87f709a40d34ea4185237536
Yang Wang <ywangd@gmail.com>
2020-07-25 22:09:35 +1000

    WIP: Working lexer

--- peek/lex.py
+++ peek/lex.py
@@ -1,7 +1,31 @@
 import re
 
-from pygments.lexer import RegexLexer, words, include
-from pygments.token import Name, Keyword, Literal, Punctuation, String, Number, Text
+from pygments.lexer import RegexLexer, words, include, bygroups
+from pygments.style import Style
+from pygments.token import Keyword, Literal, String, Number, Text, Punctuation, Name, Comment
+
+
+# null: #445
+
+class EsStyle(Style):
+    default_style = ''
+    styles = {
+        Keyword: '#d38',
+        Literal: '#499',
+        String.Symbol: '#28b',
+        String: '#395',
+        Name.Builtin: '#77f',
+        Number: '#07a'
+    }
+
+
+def innerstring_rules(ttype):
+    return [
+        # backslashes, quotes and formatting signs must be parsed one at a time
+        (r'[^\\\'"%\n]+', ttype),
+        (r'[\'"\\]', ttype),
+        # newlines are an error (use "nl" state)
+    ]
 
 
 class EsLexer(RegexLexer):
@@ -9,37 +33,21 @@ class EsLexer(RegexLexer):
     aliases = ['es']
     filenames = ['*.es']
 
-    flags = re.IGNORECASE | re.DOTALL
-
-    def innerstring_rules(ttype):
-        return [
-            # the old style '%s' % (...) string formatting
-            (r'%(\(\w+\))?[-#0 +]*([0-9]+|[*])?(\.([0-9]+|[*]))?'
-             '[hlL]?[E-GXc-giorsux%]', String.Interpol),
-            # backslashes, quotes and formatting signs must be parsed one at a time
-            (r'[^\\\'"%\n]+', ttype),
-            (r'[\'"\\]', ttype),
-            # unhandled string formatting sign
-            (r'%', ttype),
-            # newlines are an error (use "nl" state)
-        ]
+    flags = re.IGNORECASE
 
     tokens = {
         'root': [
-            (words(('GET', 'POST', 'PUT', 'DELETE'), suffix=r'\b'), Keyword, 'path'),
-        ],
-        'path': [
-            (r'\s+', Literal, 'payload'),
+            (r'^(\s*)(GET|POST|PUT|DELETE)(\s+)(\S+)(\s+|\n)',
+             bygroups(Text, Keyword, Text, Literal, Text), 'payload'),
         ],
         'payload': [
-            # (r'\{', Punctuation),
-            (words(('true', 'false'), suffix=r'\b'), Keyword.Constant),
+            (words(('true', 'false', 'null'), suffix=r'\b'), Name.Builtin),
+            (r'//.+$', Comment.Single),
+            (r'[]{}:,[]', Punctuation),
             ('"', String.Double, 'dqs'),
             ("'", String.Single, 'sqs'),
             include('numbers'),
-            # (r'[^\S\n]+', Text),
-            (r'\s+', Literal),
-
+            (r'\s+', Text),
         ],
         'strings-double': innerstring_rules(String.Double),
         'strings-single': innerstring_rules(String.Single),
@@ -63,4 +71,3 @@ class EsLexer(RegexLexer):
             (r'\d+j?', Number.Integer)
         ],
     }
-
