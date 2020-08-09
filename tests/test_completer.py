e633acad5327321b6db956efcf1c8fa9f8df4698
Yang Wang <ywangd@gmail.com>
2020-08-09 11:46:10 +1000

    Skip specs test if no specs are found

--- tests/test_completer.py
+++ tests/test_completer.py
@@ -8,6 +8,8 @@ def test_load_specs():
     package_root = os.path.dirname(package_root)
     kibana_dir = os.path.join(package_root, 'specs', 'kibana-7.8.1')
     specs = load_specs(kibana_dir)
-    # Make sure loading and merging work
-    print(specs['indices.create']['data_autocomplete_rules'])
-    print(specs['security.put_user']['data_autocomplete_rules'])
+    # Skip the test if no specs are found
+    if specs:
+        # Make sure loading and merging work
+        print(specs['indices.create']['data_autocomplete_rules'])
+        print(specs['security.put_user']['data_autocomplete_rules'])
