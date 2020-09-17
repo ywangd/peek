aca3c67f3c09afb0952e33d9119a0d502ba56811
Yang Wang <ywangd@gmail.com>
2020-09-17 20:11:40 +1000

    Attempt to make spec tests work on CI

---
+++ scripts/specs-for-ci.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+if [[ -n "$MULE_TOKEN" ]]; then
+    curl -L --remote-name -H 'Accept: application/vnd.github.v3.raw' -H "Authorization: token $MULE_TOKEN" https://api.github.com/repos/ywangd/mule/contents/kibana-7.8.1-spec-files.tgz
+    tar -C peek zxvf kibana-7.8.1-spec-files.tgz
+fi
