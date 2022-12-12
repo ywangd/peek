ca939151337f7208ba6617c7273e171fdfb84bcc
Yang Wang <yang.wang@elastic.co>
2022-12-12 17:45:52 +1100

    Fix CI properly

--- tests/test_schema_completer.py
+++ tests/test_schema_completer.py
@@ -786,7 +786,7 @@ def test_query_types():
   "query": {
     ""
   },
-}''', 32)), Completion(text='term'), Completion(text='range21'))
+}''', 32)), Completion(text='term'), Completion(text='range'))
 
 
 def equivalent_completions(c0: Completion, c1: Completion):
