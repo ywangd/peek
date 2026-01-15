80b6ae5b3f0020c97069a14b47bafd6527d25036
Quentin Pradet <quentin.pradet@elastic.co>
2026-01-15 14:14:37 +0400

    Fix spec download script (#216)

--- scripts/specs-for-ci.sh
+++ scripts/specs-for-ci.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-if [[ -n "$MULE_TOKEN" ]]; then
-    curl -L --remote-name -H 'Accept: application/vnd.github.v3.raw' -H "Authorization: token $MULE_TOKEN" https://api.github.com/repos/ywangd/mule/contents/kibana-7.8.1-spec-files.tgz
-    tar -C peek -zxvf kibana-7.8.1-spec-files.tgz
+curl -L --remote-name https://raw.githubusercontent.com/ywangd/mule/main/kibana-7.8.1-spec-files.tgz
+tar -C peek -zxvf kibana-7.8.1-spec-files.tgz
 
-    curl -L --remote-name -H 'Accept: application/vnd.github.v3.raw' -H "Authorization: token $MULE_TOKEN" https://api.github.com/repos/ywangd/mule/contents/schema-8.2.tgz
-    tar -C peek -zxvf schema-8.2.tgz
-    mv peek/specs/schema-8.2/schema.json peek/specs/.
-fi
+curl -L --remote-name https://raw.githubusercontent.com/ywangd/mule/main/schema-8.2.tgz
+tar -C peek -zxvf schema-8.2.tgz
+mv peek/specs/schema-8.2/schema.json peek/specs/schema.json
