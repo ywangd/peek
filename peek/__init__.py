1f402bd491754b7c3a9d5dcc529c6061015d626c
Quentin Pradet <quentin.pradet@elastic.co>
2026-01-24 03:35:05 +0400

    Setup Trusted Publishing (#221)

--- peek/__init__.py
+++ peek/__init__.py
@@ -1,5 +1,13 @@
 """Top-level package for peek."""
 
+from importlib.metadata import PackageNotFoundError, version
+
 __author__ = """Yang Wang"""
 __email__ = 'ywangd@gmail.com'
-__version__ = '0.4.0'
+
+
+try:
+    __version__ = version("es-peek")
+except PackageNotFoundError:
+    # package is not installed
+    pass
